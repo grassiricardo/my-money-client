b054dcfd60176ea028dfb64aab87b7d5b6402d5a
grassiricardo <grassiricardorg@gmail.com>
2017-09-27 23:59:24 -0300

    component index and app

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
     ],
     module: {
         loaders: [{
-            test: /,js[x]?$/,
+            test: /.js[x]?$/,
             loader: 'babel-loader',
             exclude: /node_modules/,
             query: {
