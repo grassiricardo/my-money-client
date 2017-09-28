f7b2eb71d2d729a27a5bd12a28f080edbe08e0e7
grassiricardo <grassiricardorg@gmail.com>
2017-09-28 00:38:46 -0300

    add footer

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2017
+            <a href="http://ricardograssi.info" target='_blank'> Ricardo Grassi </a>.
+        </strong>
+    </footer>
+)
