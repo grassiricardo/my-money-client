f5f2a6082b3eab875ae383f14a4366f07d78d311
Ricardo Grassi <grassiricardorg@gmail.com>
2017-10-28 09:11:07 -0200

    integrate reducer to dashboard

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
@@ -7,17 +8,18 @@ import Row from '../common/layout/row'
 
 class Dashboard extends Component {
     render() {
+        const { credit, debt } = this.props.sumary
         return (
             <div>
                 <ContentHeader title='Dashboard' small='Versao 1.0' />
                 <Content>
                     <Row>
                         <ValueBox cols='12 4' color='green' icon='bank'
-                            value='R$ 10' text='Total de Creditos' />
+                            value={`R$ ${credit}`} text='Total de Creditos' />
                         <ValueBox cols='12 4' color='red' icon='credit-card'
-                            value='R$ 10' text='Total de Debitos' />
+                            value={`R$ ${debt}`}  text='Total de Debitos' />
                         <ValueBox cols='12 4' color='blue' icon='money'
-                            value='R$ 0' text='Valor Consolidado' />
+                            value={`R$ ${credit - debt}`}  text='Valor Consolidado' />
                     </Row>
                 </Content>
             </div>
@@ -25,4 +27,5 @@ class Dashboard extends Component {
     }
 }
 
-export default Dashboard
+const mapStateToProps = state => ({sumary: state.dashboard.summary})
+export default connect(mapStateToProps)(Dashboard)
--- src/main/routes.jsx
+++ src/main/routes.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Router, Route, Redirect, hashHistory } from 'react-router'
 
-import Dashboard from '../dasboard/dashboard'
+import Dashboard from '../dashboard/dashboard'
 import BillingCycle from '../billingCycle/billingCycle'
 
 export default props => (
